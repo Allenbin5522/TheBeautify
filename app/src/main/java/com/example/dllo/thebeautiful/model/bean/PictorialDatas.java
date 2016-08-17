d77c94cefb8f7bd1a0b9ee965639df66269a8cc4
朱子平 <449079541@qq.com>
2016-08-17 15:57:43 +0800

    画报页

---
+++ app/src/main/java/com/example/dllo/thebeautiful/model/bean/PictorialDatas.java
@@ -0,0 +1,147 @@
+package com.example.dllo.thebeautiful.model.bean;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.squareup.picasso.Target;
+
+/**
+ * Created by dllo on 16/8/17.
+ */
+public class PictorialDatas implements Parcelable{
+    private int id;
+    private String headerTitle, link;
+    public Target target;
+    private String Content;
+    private String image_url;
+    private  String name;
+    private String label;//创始人
+    private  String sub_title;
+
+    public PictorialDatas() {
+    }
+
+    public PictorialDatas(String label, String content, String headerTitle, int id, String image_url, String link, String name, String sub_title, Target target) {
+        this.label = label;
+        Content = content;
+        this.headerTitle = headerTitle;
+        this.id = id;
+        this.image_url = image_url;
+        this.link = link;
+        this.name = name;
+        this.sub_title = sub_title;
+        this.target = target;
+    }
+
+    protected PictorialDatas(Parcel in) {
+        id = in.readInt();
+        headerTitle = in.readString();
+        link = in.readString();
+        Content = in.readString();
+        image_url = in.readString();
+        name = in.readString();
+        label = in.readString();
+        sub_title = in.readString();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(headerTitle);
+        dest.writeString(link);
+        dest.writeString(Content);
+        dest.writeString(image_url);
+        dest.writeString(name);
+        dest.writeString(label);
+        dest.writeString(sub_title);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<PictorialDatas> CREATOR = new Creator<PictorialDatas>() {
+        @Override
+        public PictorialDatas createFromParcel(Parcel in) {
+            return new PictorialDatas(in);
+        }
+
+        @Override
+        public PictorialDatas[] newArray(int size) {
+            return new PictorialDatas[size];
+        }
+    };
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String content) {
+        Content = content;
+    }
+
+    public String getHeaderTitle() {
+        return headerTitle;
+    }
+
+    public void setHeaderTitle(String headerTitle) {
+        this.headerTitle = headerTitle;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSub_title() {
+        return sub_title;
+    }
+
+    public void setSub_title(String sub_title) {
+        this.sub_title = sub_title;
+    }
+
+    public Target getTarget() {
+        return target;
+    }
+
+    public void setTarget(Target target) {
+        this.target = target;
+    }
+}
--- app/src/main/java/com/example/dllo/thebeautiful/ui/fragment/PictorialFragment.java
+++ app/src/main/java/com/example/dllo/thebeautiful/ui/fragment/PictorialFragment.java
@@ -1,14 +1,41 @@
 package com.example.dllo.thebeautiful.ui.fragment;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.appeaser.deckview.views.DeckChildView;
+import com.appeaser.deckview.views.DeckView;
 import com.example.dllo.thebeautiful.R;
-import com.example.dllo.thebeautiful.ui.fragment.AbsBaseFragment;
+import com.example.dllo.thebeautiful.model.bean.PictorialBean;
+import com.example.dllo.thebeautiful.model.bean.PictorialDatas;
+import com.example.dllo.thebeautiful.model.net.OKHttpInstance;
+import com.example.dllo.thebeautiful.model.net.OnHttpCallBack;
+import com.google.gson.Gson;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+
 
 /**
  * Created by dllo on 16/8/12.
  * 画报界面
  */
 public class PictorialFragment extends AbsBaseFragment {
+    private FrameLayout mFramLayot;
+    private static int KEY = 0;
+    DeckView<PictorialDatas> mDeckView;
+    Drawable mDefaultHeaderIcon;
+    ArrayList<PictorialDatas> mEntries;
+    Bitmap mDefaultThumbnail;
+    int scrollToChildIndex = -1;
 
+    String url = "http://design.zuimeia.com/api/v1/articles/daily/?page=1&page_size=30&user_id=0&device_id=863360020709857&platform=android&lang=zh&appVersion=1.0.4&appVersionCode=10004&systemVersion=23&countryCode=CN&user_id=0&token=&package_name=com.zuiapps.zuiworld";
 
     @Override
     protected int setLayout() {
@@ -17,14 +44,128 @@ public class PictorialFragment extends AbsBaseFragment {
 
     @Override
     protected void initViews() {
-
+        mFramLayot = (FrameLayout)getActivity().findViewById(R.id.pictorial_content);
     }
 
     @Override
     protected void initDatas() {
+        if (mEntries == null) {
+            mEntries = new ArrayList<>();
+            OKHttpInstance.getInstance().startRequest(url, new OnHttpCallBack<String>() {
+                @Override
+                public void onSuccess(String response) {
+                    Gson gson = new Gson();
+                    PictorialBean pictorialBean = gson.fromJson(response, PictorialBean.class);
+                    for (int i = 0; i < pictorialBean.getData().getArticles().size(); i++) {
+                        PictorialDatas pictorialDatas = new PictorialDatas();
+                        pictorialDatas.setLink(pictorialBean.getData().getArticles().get(i).getImage_url());
+                        pictorialDatas.setId(generateUniqueKey());
+                        String title = pictorialBean.getData().getArticles().get(i).getTitle();
+                        String subTitle = pictorialBean.getData().getArticles().get(i).getSub_title();
+                        pictorialDatas.setHeaderTitle(title);
+                        pictorialDatas.setContent(pictorialBean.getData().getArticles().get(i).getContent());
+                        pictorialDatas.setSub_title(subTitle);
+                        for (int j = 0; j < pictorialBean.getData().getArticles().get(i).getDesigners().size(); j++) {
+                            pictorialDatas.setLabel(pictorialBean.getData().getArticles().get(i).getDesigners().get(j).getCity());
+                            pictorialDatas.setName(pictorialBean.getData().getArticles().get(i).getDesigners().get(j).getName());
+                            pictorialDatas.setImage_url(pictorialBean.getData().getArticles().get(i).getDesigners().get(j).getAvatar_url());
+                        }
+                        mEntries.add(0, pictorialDatas);
+                    }
+                    mDeckView = new DeckView<>(context);
+                    mFramLayot.addView(mDeckView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
+                    DeckView.Callback<PictorialDatas> callback = new DeckView.Callback<PictorialDatas>() {
+                        @Override
+                        public ArrayList<PictorialDatas> getData() {
+                            return mEntries;
+                        }
 
+                        @Override
+                        public void loadViewData(WeakReference<DeckChildView<PictorialDatas>> dcv, PictorialDatas item) {
+                            loadViewDataInternal(item, dcv);
+                        }
+
+                        @Override
+                        public void unloadViewData(PictorialDatas item) {
+                            Picasso.with(context).cancelRequest(item.getTarget());
+                        }
+
+                        @Override
+                        public void onViewDismissed(PictorialDatas item) {
+                            mEntries.remove(item);
+                            mDeckView.notifyDataSetChanged();
+                        }
+
+                        @Override
+                        public void onItemClick(PictorialDatas item) {
+
+                        }
+
+                        @Override
+                        public void onNoViewsToDeck() {
+                            Toast.makeText(context, "没有图了", Toast.LENGTH_SHORT).show();
+                        }
+                    };
+                    mDeckView.initialize(callback);
+                    if (scrollToChildIndex != -1) {
+                        mDeckView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                mDeckView.scrollToChild(scrollToChildIndex);
+                            }
+                        });
+                    }
+
+                }
+
+                @Override
+                public void onError(Throwable ex) {
+                    Toast.makeText(context, "请求失败", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+    }
+
+    public void loadViewDataInternal(final PictorialDatas datum,
+                                     final WeakReference<DeckChildView<PictorialDatas>> weakView) {
+        Picasso.with(context).cancelRequest(datum.getTarget());
+        datum.target = new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                // Pass loaded Bitmap to view
+                if (weakView.get() != null) {
+                    weakView.get().onDataLoaded(datum, bitmap,
+                            mDefaultHeaderIcon, datum.getHeaderTitle(), Color.WHITE);
+                }
+            }
+
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) {
+                // Loading failed. Pass default thumbnail instead
+                if (weakView.get() != null) {
+                    weakView.get().onDataLoaded(datum, mDefaultThumbnail,
+                            mDefaultHeaderIcon, datum.getHeaderTitle() + " Failed", Color.WHITE);
+                }
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+                // Pass the default thumbnail for now. It will
+                // be replaced once the target Bitmap has been loaded
+                if (weakView.get() != null) {
+                    weakView.get().onDataLoaded(datum, mDefaultThumbnail,
+                            mDefaultHeaderIcon, "IsLoad...", Color.DKGRAY);
+                }
+            }
+        };
+
+        // Begin loading
+        Picasso.with(context).load(datum.getLink()).into(datum.getTarget());
     }
 
 
-
+    private static int generateUniqueKey() {
+        return ++KEY;
+    }
 }
