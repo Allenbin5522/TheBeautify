67860a915178451642dc735aa141a01af9e01e99
孙鹏 <1507438504@qq.com>
2016-08-18 16:49:49 +0800

    设计师详情上半部分

--- app/src/main/java/com/example/dllo/thebeautiful/model/net/URLValues.java
+++ app/src/main/java/com/example/dllo/thebeautiful/model/net/URLValues.java
@@ -22,11 +22,14 @@ public class URLValues {
     public static final String THINGS_ACCESSORY = "http://design.zuimeia.com/api/v1/products/category/4?page=1&page_size=30&device_id=867886023232352&platform=android&lang=zh&appVersion=1.0.6&appVersionCode=10006&systemVersion=22&countryCode=CN&user_id=0&token=&package_name=com.zuiapps.zuiworld";
     //其他
     public static final String THINGS_OTHERS = "http://design.zuimeia.com/api/v1/products/category/54?page=1&page_size=30&device_id=867886023232352&platform=android&lang=zh&appVersion=1.0.6&appVersionCode=10006&systemVersion=22&countryCode=CN&user_id=0&token=&package_name=com.zuiapps.zuiworld";
-    //设计师
+    /**
+     *设计师
+     */
+    // 设计师首页
     public static final String DESIGNER  = "http://design.zuimeia.com/api/v1/designers/?page=1&page_size=30&device_id=000000000000000&platform=android&lang=zh&appVersion=1.1.5&appVersionCode=10105&systemVersion=22&countryCode=CN&user_id=0&token=&package_name=com.zuiapps.zuiworld";
-
-
+    // 设计师详情页拼接前部分
     public static final String DESIGNER_BANNER_BEFORE = "http://design.zuimeia.com/api/v1/designer/";
+    // 设计师详情页拼接后部分
     public static final String DESIGHER_BANNER_AFTER = "/?device_id=000000000000000&platform=android&lang=zh&appVersion=1.1.5&appVersionCode=10105&systemVersion=22&countryCode=CN&user_id=0&token=&package_name=com.zuiapps.zuiworld";
 
 }
