9619d9d21c8e8d15bd872bda0cef035523ee878d
孙鹏 <1507438504@qq.com>
2016-08-22 15:22:20 +0800

    微调

--- app/src/main/java/com/example/dllo/thebeautiful/ui/activity/mine/SettingActivity.java
+++ app/src/main/java/com/example/dllo/thebeautiful/ui/activity/mine/SettingActivity.java
@@ -1,10 +1,22 @@
 package com.example.dllo.thebeautiful.ui.activity.mine;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.dllo.thebeautiful.R;
 import com.example.dllo.thebeautiful.ui.activity.AbsBaseActivity;
+
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * Created by dllo on 16/8/13.
@@ -12,6 +24,9 @@ import com.example.dllo.thebeautiful.ui.activity.AbsBaseActivity;
  */
 public class SettingActivity extends AbsBaseActivity implements View.OnClickListener {
     private ImageView imageView_return; // 返回键
+    private CircleImageView circleImageView;
+    private Bitmap head;// 头像Bitmap
+    private RelativeLayout relativeLayout;
 
     @Override
     protected int setLayout() {
@@ -21,12 +36,17 @@ public class SettingActivity extends AbsBaseActivity implements View.OnClickList
     @Override
     protected void initViews() {
         imageView_return = byView(R.id.iv_setting_return);
+        circleImageView = byView(R.id.civ_setting);
+        relativeLayout = byView(R.id.share_friends);
 
     }
 
     @Override
     protected void initDatas() {
+        ShareSDK.initSDK(this, "sharesdk的appkey");
         imageView_return.setOnClickListener(this);
+        circleImageView.setOnClickListener(this);
+        relativeLayout.setOnClickListener(this);
 
     }
 
@@ -36,6 +56,104 @@ public class SettingActivity extends AbsBaseActivity implements View.OnClickList
             case R.id.iv_setting_return:
                 finish();
                 break;
+            case R.id.civ_setting:
+                showTypeDialog();
+                break;
+            case R.id.share_friends:
+                showShare();
+                break;
         }
+    }
+
+    private void showTypeDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final AlertDialog dialog = builder.create();
+        View view = View.inflate(this, R.layout.item_image_dialog, null);
+        TextView tv1 = (TextView) view.findViewById(R.id.tv1_dialog);
+        TextView tv2 = (TextView) view.findViewById(R.id.tv2_dialog);
+        tv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(Intent.ACTION_PICK, null);
+                intent1.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent1, 1);
+                dialog.dismiss();
+            }
+        });
+        tv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.setView(view);
+        dialog.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case 1:
+                if (requestCode == 1) {
+                    cropPhoto(data.getData());// 裁剪图片
+                }
+                break;
+            case 2:
+                if (data != null) {
+                    Bundle extras = data.getExtras();
+                    head = extras.getParcelable("data");
+                    if (head != null) {
+                        circleImageView.setImageBitmap(head);// 用ImageView显示出来
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    /**
+     * 调用系统的裁剪功能
+     *
+     * @param uri
+     */
+    public void cropPhoto(Uri uri) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        intent.putExtra("crop", "true");
+        // aspectX aspectY 是宽高的比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        // outputX outputY 是裁剪图片宽高
+        intent.putExtra("outputX", 150);
+        intent.putExtra("outputY", 150);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, 2);
+    }
+    private void showShare() {
+        ShareSDK.initSDK(this);
+        OnekeyShare oks = new OnekeyShare();
+        //关闭sso授权
+        oks.disableSSOWhenAuthorize();
+        // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间等使用
+        oks.setTitle("标题");
+        // titleUrl是标题的网络链接，QQ和QQ空间等使用
+        oks.setTitleUrl("http://sharesdk.cn");
+        // text是分享文本，所有平台都需要这个字段
+        oks.setText("我是分享文本");
+        // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+        //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+        // url仅在微信（包括好友和朋友圈）中使用
+        oks.setUrl("http://sharesdk.cn");
+        // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+        oks.setComment("我是测试评论文本");
+        // site是分享此内容的网站名称，仅在QQ空间使用
+        oks.setSite(getString(R.string.app_name));
+        // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+        oks.setSiteUrl("http://sharesdk.cn");
+        // 启动分享GUI
+        oks.show(this);
     }
 }
